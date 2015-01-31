e5650e4b98d25ebce7450759ed7972ca357eb8ff
etheleon <picy2k@gmail.com>
2015-01-31 22:25:16 +0800

    processing now prints to stdout serial output from arduino

--- pi/processing.pl
+++ pi/processing.pl
@@ -1,53 +1,25 @@
 #!/usr/bin/env perl
 
-use v5.10;
+use v5.18;
 use strict;
 use warnings;
 
-use feature "switch";
-use autodie;
 use Device::SerialPort::Arduino;
-use IO::Handle;
 
-#die "Usage $0: <port>\n" unless $#ARGV == 0;
-#$port = shift;
+die "Usage $0: <port>\n
+run ls /dev/tty* before and after plugging in arduino to see which is your usb port\n" unless $#ARGV == 0;
 
-#my $port = '/dev/tty.usbserial-AJ02XSEW';  #on MAC
-my $port = '/dev/ttyUSB0';                  #on raspberrypi
+my $port = shift;
 
 my $Arduino = Device::SerialPort::Arduino->new(
     port     => $port,
-    baudrate => 115200,
+    baudrate => 9600,
     databits => 8,
     parity   => 'none',
 );
 
-open my $output, ">", "/mnt/sensor/output.txt" or die $!;
-$output->autoflush;
-
 while(1) {
-    my $line = $Arduino->receive(2);
-    say $output processing($line);
-    #say $output $line;
-    sleep(1);
-}
-sub processing
-{
-    my ($line) = @_;
-    my $value;
-    for($line)
-    {
-       when(m/Temperature: (\d+\.\d+)/)     { $value = 'Temperature: '.eval{$1-27}}
-        when(m/CO: (\d+\.\d+)/)             { $value = 'CO: '.eval{10**(-1*(log10($1/13.3)-2))}.'ppm'}
-        when(m/NO2: (\d+\.\d+)/)            { $value = 'NO2: '.eval{10**((log10($1/0.8)-6.171579)/1.707511)}.'ppm'}
-        #when(m/O3: (\d+\.\d+)/)             { $value = 'O3: '.eval{10**((log10($1/0.8)-6.171579)/1.707511)}.'ppb'} #havent figured this one out
-        default {$value=$line}
-    }
-    return $value;
-}
-
-sub log10
-{
-    my $n = shift;
-    return log($n)/log(10);
+    my $line = $Arduino->receive(1);
+    say $line;
+    sleep(1)
 }
